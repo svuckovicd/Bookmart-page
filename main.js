e4b44cc95b074ac6d8fb5b6b472b9ef7ca688a36
svuckovicd <52897195+svuckovicd@users.noreply.github.com>
2020-04-28 22:13:53 +0200

    html,sass,javascript

---
+++ main.js
@@ -0,0 +1,151 @@
+// =================== NAVBAR MENU 
+//select dom items     
+const menuBtn = document.querySelector('.nav');
+const navMenu = document.querySelector('.nav-btn')
+const navUl = document.querySelector('ul');
+const navLi = menuBtn.querySelectorAll('ul li');
+const btnLines = document.querySelectorAll('.btn-line');
+const bgItem = document.body;
+
+// NAVBAR FOR MOBILE
+
+navMenu.addEventListener('click', toggleMenu);
+
+let showMenu = false;
+function toggleMenu() {
+
+    if (!showMenu) {
+        menuBtn.classList.add('show')
+        navUl.classList.add('show')
+        bgItem.classList.add('bg-img')
+        navLi.forEach(li => li.classList.add('text'));
+        btnLines.forEach(line => line.classList.add('close'));
+        showMenu = true;
+
+    } else {
+        menuBtn.classList.remove('close')
+        navUl.classList.remove('show')
+        navLi.forEach(li => li.classList.remove('text'));
+
+        bgItem.classList.remove('bg-img')
+        btnLines.forEach(line => line.classList.remove('close'));
+        showMenu = false;
+    }
+}
+
+// ================================ MAIN
+
+// MAIN ANIMATION
+function init() {
+    const imgSlide = document.querySelector('#img');
+    const textAnim = document.querySelector('#text-anim');
+    imgSlide.classList.add('slide')
+    textAnim.classList.add('slide')
+
+
+
+}
+
+window.addEventListener('load', init)
+
+
+
+//  =========================== SECTION PAGE
+//  SECTION -- 1
+
+
+const list = document.querySelectorAll('.list .li');
+list.forEach(li => li.addEventListener('click', clickFeatures));
+
+// click Features  
+
+function clickFeatures() {
+    const pages = document.querySelectorAll('.page');
+    let panelTarget = document.querySelector(this.dataset.id);
+    list.forEach(li => {
+        li.classList.remove('active')
+    })
+    pages.forEach(page => {
+        if (panelTarget == page) {
+            page.classList.add('active');
+            this.classList.add('active')
+
+
+        } else {
+            page.classList.remove('active')
+        }
+    })
+}
+
+
+// ==================  SECTION -3
+
+// DOM ITEMS
+const arowQest = document.querySelectorAll('.arow');
+const answP = document.querySelectorAll('.answ');
+
+arowQest.forEach(ar => ar.addEventListener('click', toggleQuests));
+let showQuest = false;
+
+function toggleQuests() {
+
+    let panelTag = document.querySelector(this.dataset.qs);
+    let line = this.querySelectorAll('.line')
+    answP.forEach(answ => {
+        if (answ == panelTag) {
+            line.forEach(line => line.classList.toggle('close'));
+            if (!showQuest) {
+                answ.classList.add('show')
+                showQuest = true
+            } else {
+                answ.classList.remove('show')
+                showQuest = false
+            }
+        }
+    })
+
+}
+
+
+/*
+   CHECK VALIDATION MAIL
+*/
+
+
+const formBtn = document.forms[0].querySelector('#btn');
+const inp = document.querySelector('.inp');
+formBtn.addEventListener('click', validEmail);
+
+function validEmail(e) {
+    e.preventDefault()
+    let inpVal = inp.querySelector('input[type="email"]').value.toLowerCase();
+    console.log(inpVal)
+    let span = inp.querySelector('.sp');
+    let img = inp.querySelector('img');
+    if (inpVal == '') {
+        img.classList.add('error');
+        span.classList.add('error');
+        span.innerText = `Whoops, make sure it's an email!`;
+        return;
+    }
+    if (!inpVal.includes('@')) {
+
+        img.classList.add('error');
+        span.classList.add('error');
+        span.innerText = `Must include @!`;
+        return;
+    }
+    if (!inpVal.includes('gmail')) {
+        img.classList.add('error');
+        span.classList.add('error');
+        span.innerText = `only for gmail soory :/`;
+        return;
+    }
+    inp.style.borderColor = 'green'
+    img.classList.remove('error');
+    span.classList.remove('error');
+    span.innerHTML = 'email success!'
+    span.classList.add('succ')
+
+}
+
